b261d633ce6c638a5b867c70658046c0d9b72a93
Hung Dang <106718735+HungMika@users.noreply.github.com>
2024-06-18 10:20:23 +0700

    get data cookies + reuse code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,16 @@
         private void InitializeComponent()
         {
             this.btn_getData = new System.Windows.Forms.Button();
+            this.btn_getCookies = new System.Windows.Forms.Button();
+            this.txt_website = new System.Windows.Forms.TextBox();
+            this.txt_cookie = new System.Windows.Forms.TextBox();
+            this.lb_site = new System.Windows.Forms.Label();
+            this.lb_cookies = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_getData
             // 
-            this.btn_getData.Location = new System.Drawing.Point(12, 12);
+            this.btn_getData.Location = new System.Drawing.Point(111, 127);
             this.btn_getData.Name = "btn_getData";
             this.btn_getData.Size = new System.Drawing.Size(75, 23);
             this.btn_getData.TabIndex = 0;
@@ -41,21 +46,74 @@
             this.btn_getData.UseVisualStyleBackColor = true;
             this.btn_getData.Click += new System.EventHandler(this.btn_getData_Click);
             // 
+            // btn_getCookies
+            // 
+            this.btn_getCookies.Location = new System.Drawing.Point(195, 127);
+            this.btn_getCookies.Name = "btn_getCookies";
+            this.btn_getCookies.Size = new System.Drawing.Size(123, 23);
+            this.btn_getCookies.TabIndex = 1;
+            this.btn_getCookies.Text = "get data with cookies";
+            this.btn_getCookies.UseVisualStyleBackColor = true;
+            this.btn_getCookies.Click += new System.EventHandler(this.btn_getCookies_Click);
+            // 
+            // txt_website
+            // 
+            this.txt_website.Location = new System.Drawing.Point(111, 12);
+            this.txt_website.Name = "txt_website";
+            this.txt_website.Size = new System.Drawing.Size(148, 20);
+            this.txt_website.TabIndex = 2;
+            // 
+            // txt_cookie
+            // 
+            this.txt_cookie.Location = new System.Drawing.Point(111, 49);
+            this.txt_cookie.Name = "txt_cookie";
+            this.txt_cookie.Size = new System.Drawing.Size(148, 20);
+            this.txt_cookie.TabIndex = 3;
+            // 
+            // lb_site
+            // 
+            this.lb_site.AutoSize = true;
+            this.lb_site.Location = new System.Drawing.Point(9, 15);
+            this.lb_site.Name = "lb_site";
+            this.lb_site.Size = new System.Drawing.Size(85, 14);
+            this.lb_site.TabIndex = 4;
+            this.lb_site.Text = "Nhập trang web";
+            // 
+            // lb_cookies
+            // 
+            this.lb_cookies.AutoSize = true;
+            this.lb_cookies.Location = new System.Drawing.Point(9, 52);
+            this.lb_cookies.Name = "lb_cookies";
+            this.lb_cookies.Size = new System.Drawing.Size(72, 14);
+            this.lb_cookies.TabIndex = 5;
+            this.lb_cookies.Text = "Nhập cookies";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(330, 162);
+            this.Controls.Add(this.lb_cookies);
+            this.Controls.Add(this.lb_site);
+            this.Controls.Add(this.txt_cookie);
+            this.Controls.Add(this.txt_website);
+            this.Controls.Add(this.btn_getCookies);
             this.Controls.Add(this.btn_getData);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_getData;
+        private System.Windows.Forms.Button btn_getCookies;
+        private System.Windows.Forms.TextBox txt_website;
+        private System.Windows.Forms.TextBox txt_cookie;
+        private System.Windows.Forms.Label lb_site;
+        private System.Windows.Forms.Label lb_cookies;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,15 +15,24 @@ namespace HTTP_bai1
 {
     public partial class Form1 : Form
     {
+        private string GetWebsite;
         public Form1()
         {
             InitializeComponent();
         }
-
+        
         private void btn_getData_Click(object sender, EventArgs e)
         {
-            var html = GetData("https://howkteam.vn/");
-            TestData(html);
+            GetWebsite = txt_website.Text.ToString();
+            if (string.IsNullOrEmpty(GetWebsite))
+            {
+                MessageBox.Show("Cannot read website");
+            }
+            else
+            {
+                var html = GetData(GetWebsite);
+                TestData(html);
+            }           
         }
         
         void TestData(string html)
@@ -31,12 +40,49 @@ namespace HTTP_bai1
             File.WriteAllText("res.html", html);
             Process.Start("res.html");
         }
-        string GetData (string url)
+
+        void AddCookies(HttpRequest http,  string cookie)
+        {
+            var temp = cookie.Split(';');
+            foreach (var item in temp)
+            {
+                var tmp = item.Split('=');
+                if (tmp.Count() > 1)
+                {
+                    http.Cookies.Add(tmp[0], tmp[1]);
+                }
+            }
+        }
+        string GetData (string url, string cookie = null)
         {
             HttpRequest http = new HttpRequest();
+            http.Cookies = new CookieDictionary();
+
+            if (!string.IsNullOrEmpty(cookie)) {
+                AddCookies(http, cookie);
+            }
+            
             http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0";
             string html = http.Get(url).ToString();
             return html;
+        }
+
+        private void btn_getCookies_Click(object sender, EventArgs e)
+        {
+            GetWebsite = txt_website.Text.ToString();
+            string cookie = txt_cookie.Text.ToString();
+
+            if (string.IsNullOrEmpty(GetWebsite) && string.IsNullOrEmpty(cookie))
+            {
+                MessageBox.Show("Cannot read website and cookies!");
+            }
+            else
+            {
+                var html = GetData(GetWebsite, cookie);
+                TestData(html);
+            }
+            
+            
         }
     }
 }
